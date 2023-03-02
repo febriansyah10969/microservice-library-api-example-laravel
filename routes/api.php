26153b84c5afba881cb4f8111c5738686fe268e4
Febriansyah <febriansyah602@gmail.com>
2023-03-02 10:33:49 +0700

    BEP-16 feat: add api login

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'namespace' => 'Auth'], function () {
+    Route::post('register', 'RegisterController@register');
+    Route::post('login', 'LoginController@login');
 });
