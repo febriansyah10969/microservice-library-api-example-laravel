8a86508926a5df81d79a7b4e1d68d1d1d4644544
Febriansyah <febriansyah602@gmail.com>
2023-03-02 09:41:32 +0700

    BEP-15 fix: add pin validation and fix regex phone number

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -28,8 +28,9 @@ class RegisterRequest extends FormRequest
             'name' => 'required|min:3',
             'email' => [ 'required', 'email', 'max:255', 'unique:users,email', new Lowercase ],
             'category_id' => 'required|integer',
-            'phone' => 'required|regex:/^\08[89]\d{8}$/',
+            'phone' => ['required', 'regex:/^08(1[1-9]|2[1-2]|5[2-8]|7[1-9]|8[1-9]|9[1-9])\d{7,9}$/'],
             'date_of_birth' => 'date',
+            'pin' => 'required|digits:6|numeric|regex:/^[0-9]+$/',
         ];
     }
 
@@ -44,8 +45,12 @@ class RegisterRequest extends FormRequest
             'category_id.required' => 'Silakan isi kategori id terlebih dahulu',
             'category.id.integer' => 'Kategori id harus integer',
             'phone.required' => 'Silakan masukan nomor telepon terlebih dahulu',
-            'phone.regex' => 'Silahkan isi format no telepon seperti ini : 08******',
-            'date_of_birth.date' => 'Silahkan isi format tanggal lahir seperti ini : 01-01-2000'
+            'phone.regex' => 'Silahkan masukan nomor telephon 11-13 digit dengan format : 08******',
+            'date_of_birth.date' => 'Silahkan isi format tanggal lahir seperti ini : 01-01-2000',
+            'pin.required' => 'Silakan isi pin terlebih dahulu',
+            'pin.digits' => 'Pin harus terdiri dari 6 angka',
+            'pin.regex' => 'Silakan isi pin dengan angka',
+            'pin.numeric' => 'Silakan isi pin dengan angka'
         ];
     }
 
