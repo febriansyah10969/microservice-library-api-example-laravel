c9127dca669f98ef38280fbf01777a5430fa4aef
Febriansyah <febriansyah602@gmail.com>
2023-03-02 10:56:13 +0700

    BEP-15 fix: add grouping to route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'v1', 'namespace' => 'Auth'], function () {
-    Route::post('register', 'RegisterController@register');
+Route::group(['prefix' => 'v1'], function () {
+    Route::group(['namespace' => 'Auth'], function () {
+        Route::post('register', 'RegisterController@register');
+    });
 });
