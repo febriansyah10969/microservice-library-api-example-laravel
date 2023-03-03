afb1113b7f2881198b7c976f1d183a64d99cb363
Febriansyah <febriansyah602@gmail.com>
2023-03-03 10:02:42 +0700

    BEP-18 fix: add api update author

---
+++ app/Http/Requests/Author/UpdateAuthorRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Author;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function prepareForValidation() 
+    {
+        $this->merge(['uuid' => $this->route('uuid')]);
+    }
+
+    public function rules()
+    {
+        return [
+            'uuid' => 'exists:users,uuid',
+            'name' => 'required|min:3',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'uuid.exists' => 'author tidak ditemukan.',
+            'name.required' => 'Silakan isi email terlebih dahulu',
+            'name.min' => 'Silakan menggunakan format email yang valid',
+        ];
+    }
+}
