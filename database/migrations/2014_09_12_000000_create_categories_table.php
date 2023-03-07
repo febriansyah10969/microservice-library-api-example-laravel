37eb8cb022e6cfde4f023058cbbc7730cc2c758a
Febriansyah <febriansyah602@gmail.com>
2023-03-07 09:56:21 +0700

    change postgresql to mysql

--- database/migrations/2014_09_12_000000_create_categories_table.php
+++ database/migrations/2014_09_12_000000_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->integer('category_id')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->string('name');
             $table->timestamps();
 
